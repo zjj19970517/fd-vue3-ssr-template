e9e927af5ee526dea0932305d220aac54655f324
zhangjinjie <1392372716@qq.com>
2022-12-01 10:18:46 +0800

    feat: init vue3 ssr template

---
+++ app/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+
+const { renderToString } = require("@vue/server-renderer");
+
+const clientManifest = require("../public/dist/manifest-client.json");
+const serverManifest = require("../public/dist/manifest-server.json");
+
+const serverBundle = path.join(
+  __dirname,
+  "../public/dist",
+  serverManifest["server.js"]
+);
+
+// 这里就对标到 `entry-server.js` 导出的工厂函数
+const createApp = require(serverBundle).default;
+
+const server = express();
+
+server.use(express.static('public/dist'))
+
+server.get("/", async (req, res) => {
+  const app = createApp();
+
+  const html = await renderToString(app);
+  const clientBundle = clientManifest["client.js"];
+  res.send(`
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Vue SSR Example</title>
+    </head>
+    <body>
+      <!-- 注入组件运行结果 -->
+      <div id="app">${html}</div>
+      <!-- 注入客户端代码产物路径 -->
+      <!-- 实现 Hydrate 效果 -->
+      <script src="${clientBundle}"></script>
+    </body>
+</html>
+    `);
+});
+
+
+
+server.listen(3000, () => {
+  console.log("ready");
+});
